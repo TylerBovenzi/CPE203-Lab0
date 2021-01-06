2fe6c8816329c15060abade25eb7cacd336c38c6
Tyler Bovenzi <Tylerbovenzi4427@gmail.com>
2021-01-06 13:33:17 -0800

    Update Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -1,7 +1,27 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
 
-   }
+public class Lab00 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
+        System.out.println("x: " + x + " y: " + y + " z: " + z);
+        int[] nums = {3, 6, -1, 2};
+        for (int num : nums) {
+            System.out.println(num);
+        }
+        int numFound = char_count(y, 'l');
+        System.out.println("Found: "+numFound);        
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int char_count(String s, char c) {
+        int ans =0;
+            for (int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == c) ans++;
+            }
+        return ans;
+    }
 }
